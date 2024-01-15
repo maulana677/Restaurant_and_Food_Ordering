bd35eaf4bf0090b8421aa9d43f06f0643ce18256
maulana677 <maull0776@gmail.com>
2024-01-15 13:41:43 +0700

    memasang template admin stisla

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -10,6 +10,7 @@ class AdminDashboardController extends Controller
 {
     function index(): View
     {
-        return view('admin.dashboard.index');
+        // return view('admin.dashboard.index');
+        return view('admin.layouts.master');
     }
 }
