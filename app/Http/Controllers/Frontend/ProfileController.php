083c1bd2396a97a142d4cdc83634b30130cf3cc9
maulana677 <maull0776@gmail.com>
2024-01-18 22:20:38 +0700

    membuat fitur update password profile untuk bagian frontend

--- app/Http/Controllers/Frontend/ProfileController.php
+++ app/Http/Controllers/Frontend/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\ProfilePasswordUpdateRequest;
 use App\Http\Requests\Frontend\ProfileUpdateRequest;
 use Auth;
 use Illuminate\Http\RedirectResponse;
@@ -18,6 +19,16 @@ class ProfileController extends Controller
         $user->save();
 
         toastr()->success('Profile Updated Successfully');
+
+        return redirect()->back();
+    }
+
+    function updatePassword(ProfilePasswordUpdateRequest $request): RedirectResponse
+    {
+        $user = Auth::user();
+        $user->password = bcrypt($request->password);
+        $user->save();
+        toastr()->success('Password Updated Successfully');
 
         return redirect()->back();
     }
