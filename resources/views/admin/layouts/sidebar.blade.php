86453230f86ca2b249fca4d997bd87a48e0861f2
maulana677 <maull0776@gmail.com>
2024-01-20 21:40:31 +0700

    membuat menu slider index dan helper

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -48,9 +48,16 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class=active><a class="nav-link" href="index-0.html"><i class="fas fa-fire"></i>General Dashboard</a>
+            <li class=active><a class="nav-link" href="#"><i class="fas fa-fire"></i>General Dashboard</a>
             </li>
+
             <li class="menu-header">Starter</li>
+
+            <li>
+                <a class="nav-link" href="{{ route('admin.slider.index') }}"><i class="fas fa-images"></i>
+                    <span>Slider</span>
+                </a>
+            </li>
             {{--  <li class="dropdown">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Layout</span></a>
--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\SliderController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,4 +15,7 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('profile', [ProfileController::class, 'index'])->name('profile');
     Route::put('profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
     Route::put('profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
+
+    /** Slider Routes */
+    Route::resource('slider', SliderController::class);
 });
