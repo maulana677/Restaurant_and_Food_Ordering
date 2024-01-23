291fcb4ed5728b327c9708ee74a739803eb7ccfb
maulana677 <maull0776@gmail.com>
2024-01-23 23:15:25 +0700

    menampilkan data slider ke bagian frontend

--- app/DataTables/SliderDataTable.php
+++ app/DataTables/SliderDataTable.php
@@ -58,7 +58,7 @@ class SliderDataTable extends DataTable
             ->columns($this->getColumns())
             ->minifiedAjax()
             //->dom('Bfrtip')
-            ->orderBy(1)
+            ->orderBy(0)
             ->selectStyleSingle()
             ->buttons([
                 Button::make('excel'),
