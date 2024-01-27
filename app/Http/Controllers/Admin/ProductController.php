0924a8ba0a4960540beba34dd2e9f3a97004844d
maulana677 <maull0776@gmail.com>
2024-01-27 11:47:03 +0700

    membuat fungsi edit data dan delete

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\DataTables\ProductDataTable;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\ProductCreateRequest;
+use App\Http\Requests\Admin\ProductUpdateRequest;
 use App\Models\Category;
 use App\Models\Product;
 use App\Traits\FileUploadTrait;
@@ -12,6 +13,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
@@ -74,24 +76,55 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): View
     {
-        //
+        $categories = Category::all();
+        $product = Product::findOrFail($id);
+        return view('admin.product.edit', compact('categories', 'product'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(ProductUpdateRequest $request, string $id): RedirectResponse
     {
-        //
+        $product = Product::findOrFail($id);
+
+        /** Handle image file */
+        $imagePath = $this->uploadImage($request, 'image', $product->thumb_image);
+
+        $product->thumb_image = !empty($imagePath) ? $imagePath : $product->thumb_image;
+        $product->name = $request->name;
+        $product->category_id = $request->category;
+        $product->price = $request->price;
+        $product->offer_price = $request->offer_price ?? 0;
+        $product->quantity = $request->quantity;
+        $product->short_description = $request->short_description;
+        $product->long_description = $request->long_description;
+        $product->sku = $request->sku;
+        $product->seo_title = $request->seo_title;
+        $product->seo_description = $request->seo_description;
+        $product->show_at_home = $request->show_at_home;
+        $product->status = $request->status;
+        $product->save();
+
+        toastr()->success('Update Successfully');
+        return to_route('admin.product.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): Response
     {
-        //
+        try {
+            $product = Product::findOrFail($id);
+            $this->removeImage($product->thumb_image);
+            $product->delete();
+
+            return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+        } catch (\Exception $e) {
+            return response(['status' => 'error', 'message' => 'something went wrong!']);
+        }
     }
 }
