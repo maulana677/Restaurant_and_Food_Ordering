89a22e4f2ff08dedd43b74359979701a9d9150f5
maulana677 <maull0776@gmail.com>
2024-02-01 16:13:01 +0700

    menambahkan fitur tambah keranjang 2

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -3,12 +3,34 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class CartController extends Controller
 {
     function addToCart(Request $request)
     {
-        dd($request->all());
+        //menambahkan product ke dalam keranjang
+        $product = Product::with(['productSizes', 'productOptions'])->findOrFail($request->product_id);
+        $productSize = $product->productSizes->where('id', $request->product_size)->first();
+        $productOptions = $product->productOptions->whereIn('id', $request->product_option);
+
+        $options = [
+            'product_size' => [
+                'id' => $productSize->id,
+                'name' => $productSize->name,
+                'price' => $productSize->price,
+            ],
+            'product_options' => []
+        ];
+
+        foreach ($productOptions as $option) {
+            $options['product_options'][] = [
+                'id' => $option->id,
+                'name' => $option->name,
+                'price' => $option->price,
+            ];
+        }
     }
 }
