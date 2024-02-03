5814beadef34ac5d238229234e6f4eb050c06faf
maulana677 <maull0776@gmail.com>
2024-02-03 22:03:27 +0700

    menampilkan perhitungan keranjang dinamis 1

--- app/Helpers/global_helper.php
+++ app/Helpers/global_helper.php
@@ -21,6 +21,27 @@ if (!function_exists('generateUniqueSlug')) {
     }
 }
 
+/** Calculate cart total price */
+if (!function_exists('cartTotal')) {
+    function cartTotal()
+    {
+        $total = 0;
+
+        foreach (Cart::content() as $item) {
+            $productPrice = $item->price;
+            $sizePrice = $item->options?->product_size['price'] ?? 0;
+            $optionsPrice = 0;
+            foreach ($item->options->product_options as $option) {
+                $optionsPrice += $option['price'];
+            }
+
+            $total += ($productPrice + $sizePrice + $optionsPrice) * $item->qty;
+        }
+
+        return $total;
+    }
+}
+
 if (!function_exists('currencyPosition')) {
     function currencyPosition($price): string
     {
