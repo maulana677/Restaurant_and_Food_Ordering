8c0831eab71851cb43ce011c0ad13de4751a7ed3
maulana677 <maull0776@gmail.com>
2024-05-01 20:15:41 +0700

    fix: Membuat field database address

--- database/migrations/2024_05_01_130218_create_addresses_table.php
+++ database/migrations/2024_05_01_130218_create_addresses_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('delivery_area_id')->constrained('delivery_areas');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('email');
+            $table->string('phone');
+            $table->text('address');
+            $table->string('type');
             $table->timestamps();
         });
     }
