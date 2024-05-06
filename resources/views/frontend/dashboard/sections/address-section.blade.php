2bb6c1e1c9533a3f4ca531947ab48549533815b9
maulana677 <maull0776@gmail.com>
2024-05-06 23:19:24 +0700

    fix: Membuat tampilan edit data address

--- resources/views/frontend/dashboard/sections/address-section.blade.php
+++ resources/views/frontend/dashboard/sections/address-section.blade.php
@@ -103,93 +103,61 @@
                 </form>
             </div>
             <div class="fp_dashboard_edit_address ">
-                <form>
+                <form action="" method="POST">
+                    @csrf
+                    @method('PUT')
                     <div class="row">
                         <div class="col-12">
-                            <h4>edit address </h4>
-                        </div>
-
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="First Name">
-                            </div>
-                        </div>
-
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="Last Name">
-                            </div>
+                            <h4>Edit address</h4>
                         </div>
                         <div class="col-md-12 col-lg-12 col-xl-12">
                             <div class="fp__check_single_form">
-                                <input type="text" placeholder="Company Name (Optional)">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <select id="select_js4">
-                                    <option value="">select country</option>
-                                    <option value="">bangladesh</option>
-                                    <option value="">nepal</option>
-                                    <option value="">japan</option>
-                                    <option value="">korea</option>
-                                    <option value="">thailand</option>
+                                <select id="select_js4" name="area">
+                                    <option value="">Select Area</option>
+                                    @foreach ($deliveryAreas as $area)
+                                        <option value="{{ $area->id }}">{{ $area->area_name }}</option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-12 col-xl-6">
                             <div class="fp__check_single_form">
-                                <input type="text" placeholder="Street Address *">
+                                <input type="text" placeholder="First Name" name="first_name">
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-12 col-xl-6">
                             <div class="fp__check_single_form">
-                                <input type="text" placeholder="Apartment, suite, unit, etc. (optional)">
+                                <input type="text" placeholder="Last Name" name="last_name">
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-12 col-xl-6">
                             <div class="fp__check_single_form">
-                                <input type="text" placeholder="Town / City *">
+                                <input type="text" placeholder="Phone" name="phone">
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-12 col-xl-6">
                             <div class="fp__check_single_form">
-                                <input type="text" placeholder="State *">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="Zip *">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="text" placeholder="Phone *">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-12 col-xl-6">
-                            <div class="fp__check_single_form">
-                                <input type="email" placeholder="Email *">
+                                <input type="email" placeholder="Email" name="email">
                             </div>
                         </div>
                         <div class="col-md-12 col-lg-12 col-xl-12">
                             <div class="fp__check_single_form">
-                                <textarea cols="3" rows="4" placeholder="Address"></textarea>
+                                <textarea cols="3" rows="4" placeholder="Address" name="address"></textarea>
                             </div>
                         </div>
                         <div class="col-12">
                             <div class="fp__check_single_form check_area">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="flexRadioDefault2"
-                                        id="flexRadioDefault12">
-                                    <label class="form-check-label" for="flexRadioDefault12">
+                                    <input class="form-check-input" type="radio" name="type"
+                                        id="flexRadioDefault1" value="home">
+                                    <label class="form-check-label" for="flexRadioDefault1">
                                         home
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="flexRadioDefault2"
-                                        id="flexRadioDefault22">
-                                    <label class="form-check-label" for="flexRadioDefault22">
+                                    <input class="form-check-input" type="radio" name="type"
+                                        id="flexRadioDefault2" value="office">
+                                    <label class="form-check-label" for="flexRadioDefault2">
                                         office
                                     </label>
                                 </div>
@@ -197,8 +165,7 @@
                         </div>
                         <div class="col-12">
                             <button type="button" class="common_btn cancel_edit_address">cancel</button>
-
-                            <button type="submit" class="common_btn">update
+                            <button type="submit" class="common_btn">save
                                 address</button>
                         </div>
                     </div>
